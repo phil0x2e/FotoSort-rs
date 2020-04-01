95253e3600aab4d61d365f962bbd52a889d2dd20
phil <ph-ketteni@t-online.de>
2020-04-01 22:20:59 +0200

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use std::fs;
 use std::fs::create_dir;
 use std::path::Path;
 
-struct Arguments {
-    paths: Vec<String>,
+struct Config {
+    file_paths: Vec<String>,
     is_move: bool,
 }
 
-fn get_commandline_arguments() -> Arguments {
+fn get_commandline_arguments() -> Config {
     let description = "A Tool for sorting images into different folders.\n\n\
                        Just pass the images as arguments and copy the currently displayed image into the folder fs1-fs5, using the keys 1-5.\n\
                        To rotate the preview by 90° press R, the image file is not rotated.\n\
@@ -45,8 +45,8 @@ fn get_commandline_arguments() -> Arguments {
         .map(|s| String::from(s))
         .collect();
 
-    Arguments {
-        paths: files,
+    Config {
+        file_paths: files,
         is_move: matches.is_present("move"),
     }
 }
@@ -71,58 +71,58 @@ fn copy_or_move_file(from: &str, to: &str, is_move: bool) -> Result<(), std::io:
 
 fn check_user_input(
     window: &mut ImageWindow,
-    file_paths: &mut Vec<String>,
+    config: &mut Config,
     pos: &mut usize,
     is_move: bool,
 ) -> bool {
     let mut refresh = false;
     if window.is_key_pressed(Key::Key1, KeyRepeat::No) {
         let dir_name = "fs1";
-        if let Err(_e) = copy_or_move_file(&file_paths[*pos], dir_name, is_move) {
+        if let Err(_e) = copy_or_move_file(&config.file_paths[*pos], dir_name, is_move) {
             println!("Error creating directory {}", dir_name);
         }
         if is_move {
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
             refresh = true;
         }
     } else if window.is_key_pressed(Key::Key2, KeyRepeat::No) {
         let dir_name = "fs2";
-        if let Err(_e) = copy_or_move_file(&file_paths[*pos], dir_name, is_move) {
+        if let Err(_e) = copy_or_move_file(&config.file_paths[*pos], dir_name, is_move) {
             println!("Error creating directory {}", dir_name);
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
         }
         if is_move {
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
             refresh = true;
         }
     } else if window.is_key_pressed(Key::Key3, KeyRepeat::No) {
         let dir_name = "fs3";
-        if let Err(_e) = copy_or_move_file(&file_paths[*pos], dir_name, is_move) {
+        if let Err(_e) = copy_or_move_file(&config.file_paths[*pos], dir_name, is_move) {
             println!("Error creating directory {}", dir_name);
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
         }
         if is_move {
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
             refresh = true;
         }
     } else if window.is_key_pressed(Key::Key4, KeyRepeat::No) {
         let dir_name = "fs4";
-        if let Err(_e) = copy_or_move_file(&file_paths[*pos], dir_name, is_move) {
+        if let Err(_e) = copy_or_move_file(&config.file_paths[*pos], dir_name, is_move) {
             println!("Error creating directory {}", dir_name);
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
         }
         if is_move {
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
             refresh = true;
         }
     } else if window.is_key_pressed(Key::Key5, KeyRepeat::No) {
         let dir_name = "fs5";
-        if let Err(_e) = copy_or_move_file(&file_paths[*pos], dir_name, is_move) {
+        if let Err(_e) = copy_or_move_file(&config.file_paths[*pos], dir_name, is_move) {
             println!("Error creating directory {}", dir_name);
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
         }
         if is_move {
-            file_paths.remove(*pos);
+            config.file_paths.remove(*pos);
             refresh = true;
         }
     }
@@ -134,13 +134,13 @@ fn check_user_input(
     if window.is_key_pressed(Key::Delete, KeyRepeat::No) {
         println!(
             "Are you sure you want to delete {}? Yes: Y; No: N",
-            &file_paths[*pos]
+            &config.file_paths[*pos]
         );
         while window.is_open() {
             if window.is_key_pressed(Key::Y, KeyRepeat::No) {
-                println!("Deleting {}", &file_paths[*pos]);
-                fs::remove_file(&file_paths[*pos]).ok();
-                file_paths.remove(*pos);
+                println!("Deleting {}", &config.file_paths[*pos]);
+                fs::remove_file(&config.file_paths[*pos]).ok();
+                config.file_paths.remove(*pos);
                 refresh = true;
                 break;
             }
@@ -158,11 +158,11 @@ fn check_user_input(
         if *pos != 0 {
             *pos -= 1;
         } else {
-            *pos = file_paths.len() - 1;
+            *pos = config.file_paths.len() - 1;
         }
     } else if window.is_key_pressed(Key::Right, KeyRepeat::No) {
         refresh = true;
-        if *pos != file_paths.len() - 1 {
+        if *pos != config.file_paths.len() - 1 {
             *pos += 1;
         } else {
             *pos = 0;
@@ -171,14 +171,48 @@ fn check_user_input(
     refresh
 }
 
+fn window_loop(mut window: &mut ImageWindow, mut config: &mut Config) {
+    let mut pos: usize = 0;
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let mut is_move = config.is_move;
+        if config.file_paths.len() > 0 {
+            if window.is_key_down(Key::M) {
+                is_move = true;
+            }
+            if window.is_key_down(Key::C) {
+                is_move = false;
+            }
+            let refresh = check_user_input(&mut window, &mut config, &mut pos, is_move);
+            if refresh {
+                if config.file_paths.len() == 0 {
+                    println!("All images moved.");
+                    break;
+                }
+                if pos >= config.file_paths.len() {
+                    pos = config.file_paths.len() - 1;
+                }
+                println!("Image {}/{}", pos + 1, config.file_paths.len());
+                window
+                    .set_image_from_path_fit(&config.file_paths[pos])
+                    .unwrap();
+            }
+            window.fit_to_screen();
+        }
+        window.update();
+    }
+}
+
 fn main() {
-    let arguments = get_commandline_arguments();
-    let mut file_paths = arguments.paths;
+    let config = get_commandline_arguments();
     // Check if file exists and is an image, that can be opened
-    file_paths = file_paths
-        .into_iter()
-        .filter(|fp| image::open(fp).is_ok())
-        .collect();
+    let mut config = Config {
+        file_paths: config
+            .file_paths
+            .into_iter()
+            .filter(|fp| image::open(fp).is_ok())
+            .collect(),
+        ..config
+    };
 
     let mut window = ImageWindow::new(
         "FotoSort-rs",
@@ -196,36 +230,13 @@ fn main() {
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    //let images = load_images(&file_paths);
-    let mut pos: usize = 0;
-    if file_paths.len() > 0 {
-        window.set_image_from_path_fit(&file_paths[pos]).unwrap();
-        println!("Image {}/{}", 1, file_paths.len());
+    if config.file_paths.len() > 0 {
+        window
+            .set_image_from_path_fit(&config.file_paths[0])
+            .unwrap();
+        println!("Image {}/{}", 1, config.file_paths.len());
     }
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        let mut is_move = arguments.is_move;
-        if file_paths.len() > 0 {
-            if window.is_key_down(Key::M) {
-                is_move = true;
-            }
-            if window.is_key_down(Key::C) {
-                is_move = false;
-            }
-            let refresh = check_user_input(&mut window, &mut file_paths, &mut pos, is_move);
-            if refresh {
-                if file_paths.len() == 0 {
-                    println!("All images moved.");
-                    break;
-                }
-                if pos >= file_paths.len() {
-                    pos = file_paths.len() - 1;
-                }
-                println!("Image {}/{}", pos + 1, file_paths.len());
-                window.set_image_from_path_fit(&file_paths[pos]).unwrap();
-            }
-            window.fit_to_screen();
-        }
-        window.update();
-    }
+
+    window_loop(&mut window, &mut config);
     println!("\nBye.");
 }
